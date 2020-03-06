f4347fa11fd62c7f5c14a381d9702e1f5035fc82
litefeel <litefeel@gmail.com>
2020-03-06 22:34:04 +0800

    Support reset gloab variable

--- Editor/defaultlua.lua
+++ Editor/defaultlua.lua
@@ -1,10 +1,27 @@
-local function clearfile(path, needRequire)
+local function clearfile(path, needRequire, resetGValueOrName)
     path = string.gsub(path, "[\\/]", ".")
     path = string.gsub(path, "%.lua$", "")
-    print(path)
+    -- print(path)
     package.loaded[path] = nil
-    if needRequire then
-        require(path)
+    if not needRequire then return end
+
+    local gValName = resetGValueOrName
+    if resetGValueOrName == true then
+        gValName = string.gsub( path, ".*%.", "")
+    end
+    local gValue = gValName and _G[gValName]
+
+    require(path)
+
+    if gValue and type(gValue) == "table" then
+        local newGVal = _G[gValName]
+        if type(newGVal) == "table" then
+            for k, v in pairs(newGVal) do
+                if type(v) ~= "function" then
+                    newGVal[k] = v
+                end
+            end
+        end
     end
 end
 
