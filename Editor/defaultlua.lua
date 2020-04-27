814287714964eba09f64ec15a2a17c3b36a86853
litefeel <litefeel@gmail.com>
2020-04-27 22:02:35 +0800

    Update defaultlua.lua

--- Editor/defaultlua.lua
+++ Editor/defaultlua.lua
@@ -1,13 +1,32 @@
-local function clearfile(path, needRequire, resetGValueOrName)
+local clearfile
+local path2chunkname
+local mt = {}
+local globalT = setmetatable({}, mt)
+mt.__index = function(t, k)
+    if k == "clearfile" then
+        return clearfile
+    elseif k == "path2chunkname" then
+        return path2chunkname
+    else
+        return _G[k]
+    end
+end
+
+path2chunkname = function(path)
     path = string.gsub(path, "[\\/]", ".")
     path = string.gsub(path, "%.lua$", "")
+    return path
+end
+
+clearfile = function(path, needRequire, resetGValueOrName)
+    path = path2chunkname(path)
     -- print(path)
     package.loaded[path] = nil
     if not needRequire then return end
 
     local gValName = resetGValueOrName
     if resetGValueOrName == true then
-        gValName = string.gsub( path, ".*%.", "")
+        gValName = string.gsub(path, ".*%.", "")
     end
     local gValue = gValName and _G[gValName]
 
@@ -22,6 +41,13 @@ local function clearfile(path, needRequire, resetGValueOrName)
                 end
             end
         end
+    end
+end
+
+local function _loadfile(path)
+    local chunk = loadfile(path)
+    if chunk then
+        setfenv(chunk, globalT)()
     end
 end
 
